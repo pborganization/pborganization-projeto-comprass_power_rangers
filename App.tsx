6ba1c92b649706eb72be3a04c738dbaf8375bf64
Hiali <hialidias@gmail.com>
2023-10-12 17:39:44 -0300

    feat: navigation added

--- App.tsx
+++ App.tsx
@@ -1,11 +1,18 @@
 import { StyleSheet, Text, View } from "react-native";
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
+import { AdressForm } from "./src/auth/AddressForm";
 
 
 export default function App() {
+
+  const Stack = createNativeStackNavigator();
+
   return (
     <View style={styles.container}>
+      <NavigationContainer> 
+    <Stack.Screen name="Login" component={AdressForm}  options={{  headerShadowVisible: false }}/>
+      </NavigationContainer>
     </View>
   );
 }

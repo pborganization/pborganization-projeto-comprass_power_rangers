0eaf7fa6b14227866a0a4f9bfdf515868716355e
EduSalomao <eduardosalomao33@gmail.com>
2023-10-12 20:16:38 -0300

    fix: resolving conflicts

--- App.tsx
+++ App.tsx
@@ -1,9 +1,11 @@
-import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { HomeNavigator } from "./src/routes/homeRoutes";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-    </View>
->>>>>>> main
+    <NavigationContainer>
+      <HomeNavigator />
+    </NavigationContainer>
   );
 }

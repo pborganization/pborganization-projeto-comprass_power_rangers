9324d0776060ede2c454a736e49cf279dd709477
Murilo Anjos <com>
2023-10-15 20:59:49 -0300

    feat: create left arrow component

---
+++ src/components/LeftArrow/index.tsx
@@ -0,0 +1,10 @@
+import { LeftIcon } from '../Icons/LeftArrow';
+import { Container } from './styles';
+
+export function LeftArrow() {
+  return (
+    <Container>
+      <LeftIcon/>
+    </Container>
+  );
+}

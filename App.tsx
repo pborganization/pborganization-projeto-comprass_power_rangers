289b6a54854633ee7d80ba2cc3d72c922001cd68
Murilo Anjos <com>
2023-10-16 10:44:14 -0300

    feat: create forgotpassword screen

--- App.tsx
+++ App.tsx
@@ -1,11 +1,31 @@
-import { StyleSheet, Text, View } from "react-native";
-import { MainScreen } from "./src/screens/mainScreen";
+import { useFonts } from 'expo-font';
+import { StyleSheet, View } from 'react-native';
+import { MainScreen } from './src/screens/mainScreen';
+import { LoginScreen } from './src/screens/Login';
+import { SignUpScreen } from './src/screens/SingUp';
+import { ForgotPasswordScreen } from './src/screens/ForgotPassword';
+import { StatusBar } from 'expo-status-bar';
 
 export default function App() {
+  const [isFontsLoaded] = useFonts({
+    'OpenSans-400': require('./src/assets/fonts/OpenSans-Regular.ttf'),
+    'OpenSans-600': require('./src/assets/fonts/OpenSans-SemiBold.ttf'),
+    'OpenSans-700': require('./src/assets/fonts/OpenSans-Bold.ttf'),
+    'OpenSans-800': require('./src/assets/fonts/OpenSans-ExtraBold.ttf'),
+  });
+
+  if (!isFontsLoaded){
+    return null;
+  }
+
   return (
-    <View style={styles.container}>
-      <MainScreen />
-    </View>
+    <>
+      <StatusBar style='light' backgroundColor='#111213'/>
+      <View style={styles.container}>
+        {/* <MainScreen /> */}
+        <ForgotPasswordScreen />
+      </View>
+    </>
   );
 }
 
@@ -14,3 +34,4 @@ const styles = StyleSheet.create({
     flex: 1,
   },
 });
+
